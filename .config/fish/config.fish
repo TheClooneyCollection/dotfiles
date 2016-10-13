d31aaba78fbf8e81e2fcbe3ff7c25c2d9de1172d
NicholasTD07 <Nicholas.TD07@gmail.com>
2016-10-13 19:29:01 +1100

    [fish] Add my prompt `~ (master↑2|✚1) 4ae6cf9 $`

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,6 +3,9 @@ set -x PATH $PATH ~/bin/
 alias g 'git'
 alias r 'reload'
 
+#### Notes ####
+# git diff --no-prefix (echo "$config_old" | psub) (echo "$config_new" | psub)
+
 function reload
     echo Reloaded ~/.config/fish/config.fish
     . ~/.config/fish/config.fish
@@ -16,5 +19,76 @@ function vc
     vim (config_path)
 end
 
-#### Notes ####
-# git diff --no-prefix (echo "$config_old" | psub) (echo "$config_new" | psub)
+function fish_prompt
+    if not set -q __fish_git_prompt_show_informative_status
+        set -g __fish_git_prompt_show_informative_status 1
+    end
+    if not set -q __fish_git_prompt_hide_untrackedfiles
+        set -g __fish_git_prompt_hide_untrackedfiles 1
+    end
+
+    if not set -q __fish_git_prompt_color_branch
+        set -g __fish_git_prompt_color_branch magenta --bold
+    end
+    if not set -q __fish_git_prompt_showupstream
+        set -g __fish_git_prompt_showupstream "informative"
+    end
+    if not set -q __fish_git_prompt_color_dirtystate
+        set -g __fish_git_prompt_color_dirtystate blue
+    end
+    if not set -q __fish_git_prompt_color_stagedstate
+        set -g __fish_git_prompt_color_stagedstate yellow
+    end
+    if not set -q __fish_git_prompt_color_invalidstate
+        set -g __fish_git_prompt_color_invalidstate red
+    end
+    if not set -q __fish_git_prompt_color_untrackedfiles
+        set -g __fish_git_prompt_color_untrackedfiles $fish_color_normal
+    end
+    if not set -q __fish_git_prompt_color_cleanstate
+        set -g __fish_git_prompt_color_cleanstate green --bold
+    end
+
+    set -l last_status $status
+
+    if not set -q __fish_prompt_normal
+        set -g __fish_prompt_normal (set_color normal)
+    end
+
+    set -l color_cwd
+    set -l prefix
+    switch $USER
+    case root toor
+        if set -q fish_color_cwd_root
+            set color_cwd $fish_color_cwd_root
+        else
+            set color_cwd $fish_color_cwd
+        end
+        set suffix '#'
+    case '*'
+        set color_cwd $fish_color_cwd
+        set suffix '$'
+    end
+
+    # PWD
+    set_color $color_cwd
+    echo -n (prompt_pwd)
+    set_color normal
+
+    printf '%s ' (__fish_vcs_prompt)
+
+    if not test $last_status -eq 0
+    set_color $fish_color_error
+    end
+
+    set_color yellow
+
+    set commit (command git rev-parse --short HEAD)
+    echo -n "$commit "
+
+    set_color normal
+
+    echo -n "$suffix "
+
+    set_color normal
+end
