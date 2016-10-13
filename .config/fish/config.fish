2fb3990129ab42e87aa3f837d0ec905301bf1a62
NicholasTD07 <Nicholas.TD07@gmail.com>
2016-10-13 17:20:37 +1100

    [fish] Add ~/bin, `reload`, `config_path`, `vc` to edit fish's config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,1 +1,20 @@
+set -x PATH $PATH ~/bin/
+
 alias g 'git'
+alias r 'reload'
+
+function reload
+    echo Reloaded ~/.config/fish/config.fish
+    . ~/.config/fish/config.fish
+end
+
+function config_path
+    status -f
+end
+
+function vc
+    vim (config_path)
+end
+
+#### Notes ####
+# git diff --no-prefix (echo "$config_old" | psub) (echo "$config_new" | psub)
