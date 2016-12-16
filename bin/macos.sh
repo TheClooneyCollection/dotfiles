96396bcf0e2c18d209ff7e0e5a9101d604531db0
NicholasTD07 <Nicholas.TD07@gmail.com>
2016-12-16 15:09:06 +1100

    [bin] macos.sh can set up and config a Mac now
    - bootstrap brew
    - install all cask, apps, and brews
    - config for no bouncing dock icons

--- bin/macos.sh
+++ bin/macos.sh
@@ -1,12 +1,31 @@
 #!/bin/sh
 
 main() {
+    setUp
+    config
+}
+
+setUp() {
+    echo "starting setup..."
+    echo "bootstrapping brew..."
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+    echo "installing brews..."
+    brew bundle --global -v
+
+    echo "setup is done!"
+}
+
+config() {
+    echo "starting config..."
     no_bouncing_dock_icons
+    echo "config is done!"
 }
 
 no_bouncing_dock_icons() {
     defaults write com.apple.dock no-bouncing -bool TRUE
     killall Dock
+    echo "disabled bouncing icons. 😝"
 }
 
 main
