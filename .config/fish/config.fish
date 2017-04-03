05beaeada550a3dc7cbef1abb5e5d5a8447a9c56
NicholasTD07 <Nicholas.TD07@gmail.com>
2017-04-03 13:32:35 +1000

    [fish] Add alias to update casks

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -10,6 +10,7 @@ fzf_key_bindings
 
 alias b 'bundle'
 alias bb 'brew bundle --global'
+alias update_casks 'brew cask outdated | cut -d " " -f 1 | xargs brew cask install --force'
 alias o 'open'
 alias xo 'open -a Xcode *.xcworkspace'
 alias oo 'open .'
