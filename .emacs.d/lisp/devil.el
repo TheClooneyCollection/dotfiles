28e0f2be7869448e5fcfb4188eb26d383b480844
NicholasTD07 <NicholasTD07@users.noreply.github.com>
2017-07-13 15:05:15 +1000

    [emacs] : acts as ; and vice versa

--- .emacs.d/lisp/devil.el
+++ .emacs.d/lisp/devil.el
@@ -1,12 +1,17 @@
 ; VIM! I mean EVIL!
 
 (use-package evil
-  :init (setq evil-want-C-u-scroll t) ; Enable <c-u> to scroll up
-  :config (evil-mode))
+  :init
+    (setq evil-want-C-u-scroll t) ; Enable <c-u> to scroll up
+  :config
+    (define-key evil-normal-state-map ";" #'evil-ex)
+    (define-key evil-normal-state-map ":" #'evil-repeat-find-char)
+    (evil-mode))
 
 (use-package evil-escape
   :diminish evil-escape-mode
   :init (setq-default evil-escape-key-sequence "kj")
-  :config (evil-escape-mode))
+  :config
+  (evil-escape-mode))
 
 (provide 'devil)
