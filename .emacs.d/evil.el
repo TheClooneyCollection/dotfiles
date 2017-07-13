bb77842c4da2fd8ee66f52a14c499eb48d443920
NicholasTD07 <NicholasTD07@users.noreply.github.com>
2017-07-13 10:30:58 +1000

    [emacs] Add evil-escape package (so we have `kj` as escape)

--- .emacs.d/evil.el
+++ .emacs.d/evil.el
@@ -5,3 +5,7 @@
   :init (setq evil-want-C-u-scroll t) ; Enable <c-u> to scroll up
   :config (evil-mode))
 
+(use-package evil-escape
+  :diminish evil-escape-mode
+  :init (setq-default evil-escape-key-sequence "kj")
+  :config (evil-escape-mode))
