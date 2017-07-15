200930f4724c1d2897c4009c660e88a6c07d05d5
NicholasTD07 <NicholasTD07@users.noreply.github.com>
2017-07-16 09:50:03 +1000

    [emacs] Set up display-time-mode and display-time-world-list

--- .emacs.d/lisp/ui.el
+++ .emacs.d/lisp/ui.el
@@ -13,4 +13,22 @@
   :init (setq whitespace-style '(face tabs trailing empty tab-mark))
   :config (global-whitespace-mode))
 
+(use-package time ; Built-in
+  :diminish display-time-mode
+  :init
+  (general-define-key "it" 'display-time-world)
+  (setq display-time-world-list '(
+                                  ("Australia/Sydney" "Sydney")
+                                  ("Asia/Chongqing" "Chongqing")
+                                  ("PST8PDT" "San Francisco")
+                                  ("Asia/Calcutta" "Bangalore")
+                                  ("Australia/Melbourne" "Melbourne")
+                                  ("Europe/London" "London")
+                                  ("Europe/Paris" "Paris")
+                                  ("Asia/Tokyo" "Tokyo")
+                                  ("America/Los_Angeles" "Los Angeles")
+                                  ("America/New_York" "New York")
+                                  ))
+  :config (display-time-mode))
+
 (provide 'ui)
