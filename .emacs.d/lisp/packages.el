adcfd39d2c8b573dceef3f515d18dd42b8211a8b
NicholasTD07 <NicholasTD07@users.noreply.github.com>
2017-07-17 22:25:46 +1000

    [emacs] Move configurations for keys UP1

--- .emacs.d/lisp/packages.el
+++ .emacs.d/lisp/packages.el
@@ -44,6 +44,31 @@
     (general-define-key "eu" 'dot-emacs/edit-ui)
 )
 
+(use-package evil-easymotion
+  :after evil
+  :config
+  ; Evil-easymotion's line movements work perfectly with evil.
+  (general-define-key "j" (evilem-create 'evil-next-line))
+  (general-define-key "k" (evilem-create 'evil-previous-line))
+)
+
+; Avy's line movements do not work with evil too well
+; For example, when in visual line selection mode (V), avy does not work at all;
+; and when it does work in visual selection mode (v), it selects the line before selection.
+(use-package avy
+  :commands (avy-goto-char-2 avy-goto-line-above avy-goto-line-below)
+  :bind (
+        :map evil-normal-state-map
+        ("f" . avy-goto-char-2)
+        :map evil-visual-state-map
+        ("f" . avy-goto-char-2)
+        :map evil-operator-state-map
+        ("f" . avy-goto-char-2))
+  :init
+  (setq avy-background t)
+  (setq avy-keys (number-sequence ?a ?z))
+  )
+
 ; Languages
 
 (use-package swift-mode
@@ -89,31 +114,6 @@
 (use-package magit
   :commands magit-status
   :init (general-define-key "s" 'magit-status))
-
-(use-package evil-easymotion
-  :after evil
-  :config
-  ; Evil-easymotion's line movements work perfectly with evil.
-  (general-define-key "j" (evilem-create 'evil-next-line))
-  (general-define-key "k" (evilem-create 'evil-previous-line))
-)
-
-; Avy's line movements do not work with evil too well
-; For example, when in visual line selection mode (V), avy does not work at all;
-; and when it does work in visual selection mode (v), it selects the line before selection.
-(use-package avy
-  :commands (avy-goto-char-2 avy-goto-line-above avy-goto-line-below)
-  :bind (
-        :map evil-normal-state-map
-        ("f" . avy-goto-char-2)
-        :map evil-visual-state-map
-        ("f" . avy-goto-char-2)
-        :map evil-operator-state-map
-        ("f" . avy-goto-char-2))
-  :init
-  (setq avy-background t)
-  (setq avy-keys (number-sequence ?a ?z))
-  )
 
 (use-package auto-complete
   :diminish auto-complete-mode
