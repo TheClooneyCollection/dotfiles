982af43e45105f4e5315aa84b04b9d018eacbe70
NicholasTD07 <NicholasTD07@users.noreply.github.com>
2017-07-22 10:34:43 +1000

    [emacs] Move UI configuration before loading packages

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -3,6 +3,17 @@
 (setq-default indent-tabs-mode nil) ; Don't indent with tabs.
 (setq custom-file "~/.emacs.d/custom.el")
 
+; UI
+(setq
+  ring-bell-function #'ignore
+  inhibit-startup-screen t ; Skip the startup screen
+  initial-scratch-message "Hello there!\nStart happy hacking!\n")
+
+(fset 'yes-or-no-p #'y-or-n-p) ; Change yes/no -> y/n
+(fset 'display-startup-echo-area-message #'ignore) ; No more startup message
+
+(menu-bar-mode -1) ; Hide menu bar at top
+
 ; Load packages
 (require 'package)
 (setq package-enable-at-startup nil
@@ -16,5 +27,5 @@
                    (load-file (concat (file-name-as-directory dir) f)))
                  ))
     (mapc load-it (directory-files dir nil "\\.el$"))))
-(load-directory "~/.emacs.d/lisp/")
 
+(load-directory "~/.emacs.d/lisp/")
--- .emacs.d/lisp/ui.el
+++ .emacs.d/lisp/ui.el
@@ -1,13 +1,3 @@
-(setq
-  ring-bell-function #'ignore
-  inhibit-startup-screen t ; Skip the startup screen
-  initial-scratch-message "Hello there!\nStart happy hacking!\n")
-
-(fset 'yes-or-no-p #'y-or-n-p) ; Change yes/no -> y/n
-(fset 'display-startup-echo-area-message #'ignore) ; No more startup message
-
-(menu-bar-mode -1) ; Hide menu bar at top
-
 (use-package whitespace ; Built-in
   :diminish (whitespace-mode global-whitespace-mode)
   :init (setq whitespace-style '(face tabs trailing empty tab-mark))
