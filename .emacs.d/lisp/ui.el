af39b0b9f230424d8f8a66c96dfd391e0064aaeb
NicholasTD07 <NicholasTD07@users.noreply.github.com>
2017-08-05 17:01:29 +1000

    [emacs] Fix airline messed up glyphs in powerline

--- .emacs.d/lisp/ui.el
+++ .emacs.d/lisp/ui.el
@@ -26,4 +26,13 @@
 
 (use-package airline-themes
   :after powerline
+  :init (setq powerline-utf-8-separator-left        #xe0b0
+              powerline-utf-8-separator-right       #xe0b2
+              airline-utf-glyph-separator-left      #xe0b0
+              airline-utf-glyph-separator-right     #xe0b2
+              airline-utf-glyph-subseparator-left   #xe0b1
+              airline-utf-glyph-subseparator-right  #xe0b3
+              airline-utf-glyph-branch              #xe0a0
+              airline-utf-glyph-readonly            #xe0a2
+              airline-utf-glyph-linenumber          #xe0a1)
   :config (load-theme 'airline-light t))
