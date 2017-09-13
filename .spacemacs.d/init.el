5c8403a33a0d8da0940cb2eb3382b9e5b3cc0c09
NicholasTD07 <NicholasTD07@users.noreply.github.com>
2017-09-13 10:55:44 +1000

    [spacemacs] Add tox package and set default runner to py.test

--- .spacemacs.d/init.el
+++ .spacemacs.d/init.el
@@ -69,7 +69,12 @@ values."
    ;; wrapped in a layer. If you need some configuration for these
    ;; packages, then consider creating a layer. You can also put the
    ;; configuration in `dotspacemacs/user-config'.
-   dotspacemacs-additional-packages '(company-flx helm-ls-git)
+   dotspacemacs-additional-packages
+   '(
+     company-flx
+     helm-ls-git
+     tox
+     )
    ;; A list of packages that cannot be updated.
    dotspacemacs-frozen-packages '()
    ;; A list of packages that will not be installed and loaded.
