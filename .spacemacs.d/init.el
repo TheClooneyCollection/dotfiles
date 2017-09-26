72a204eeb83bdd522664be14c605f39448633034
NicholasTD07 <NicholasTD07@users.noreply.github.com>
2017-09-27 09:00:51 +1000

    [spacemacs] Smaller fonts look better (esp. on laptop's screens)

--- .spacemacs.d/init.el
+++ .spacemacs.d/init.el
@@ -156,7 +156,7 @@ values."
    ;; Default font, or prioritized list of fonts. `powerline-scale' allows to
    ;; quickly tweak the mode-line size to make separators look not too crappy.
    dotspacemacs-default-font '("Source Code Pro"
-                               :size 20
+                               :size 17
                                :weight normal
                                :width normal
                                :powerline-scale 1.1)
