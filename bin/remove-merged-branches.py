c3ac424da8f0f884265236f756241ce0ccf1781d
NicholasTD07 <NicholasTD07@users.noreply.github.com>
2019-01-08 13:42:31 +1100

    [bin] Quick & half-ass effort towards removing remote merged branches w/ a TODO

--- bin/remove-merged-branches.py
+++ bin/remove-merged-branches.py
@@ -12,6 +12,10 @@
 # Exclude branches defined in IGNORE_BRANCHES from the branches in last step
 # Delete branches with `git branch -d`
 
+# TODO: To find out merged remote branches
+# Run command `git branch -r -merged`.
+# Its output would also need a different parser.
+
 import sys
 import subprocess
 
@@ -47,9 +51,13 @@ def filter_branches_with_ignores(branches, ignores):
                 )
             ]
 
-def delete_branch(branch):
+def delete_branch(branch, remote=False):
+    if remote:
+        command = ['git', 'push', 'origin', '--delete', branch]
+    else:
+        command = ['git', 'branch', '-d', branch]
     try:
-        output = subprocess.check_output(['git', 'branch', '-d', branch])
+        output = subprocess.check_output(command)
     except subprocess.CalledProcessError as e:
         print("returned non-zero exit status {}".format(e.returncode))
         return
