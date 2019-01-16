46da5ca20f46686a24893f71c1dab2fae6a784ad
NicholasTD07 <NicholasTD07@users.noreply.github.com>
2019-01-17 10:09:08 +1100

    [spacemacs] Add csharp layer

--- .spacemacs.d/init.el
+++ .spacemacs.d/init.el
@@ -43,6 +43,7 @@ This function should only modify configuration layer settings."
      swift
 
      ;; Minor languages
+     csharp
      html
      csv
      (javascript :variables javascript-backend 'tern)
