05580e4f10dc0b539115489433214ac12c498d00
NicholasTD07 <NicholasTD07@users.noreply.github.com>
2019-07-08 09:05:00 +1000

    [spacemacs] Prefer light mode

--- .spacemacs.d/init.el
+++ .spacemacs.d/init.el
@@ -214,8 +214,8 @@ It should only modify the values of Spacemacs settings."
    ;; Press `SPC T n' to cycle to the next theme in the list (works great
    ;; with 2 themes variants, one dark and one light)
    dotspacemacs-themes '(
-                         spacemacs-dark
                          spacemacs-light
+                         spacemacs-dark
                          tsdh-light
                          )
 
