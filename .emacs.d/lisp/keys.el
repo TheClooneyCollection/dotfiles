757bed37ac8e911f18bc3e09efe63d63cd0d731a
NicholasClooney <137233165+NicholasClooney@users.noreply.github.com>
2026-05-22 08:32:56 +0100

    feat(emacs): restore spacemacs-style leader and evil keys

--- .emacs.d/lisp/keys.el
+++ .emacs.d/lisp/keys.el
@@ -12,6 +12,16 @@
 (use-package general
   :after evil
   :config
+  (defun open-init-file ()
+    "Open the main init file."
+    (interactive)
+    (find-file (expand-file-name "init.el" user-emacs-directory)))
+
+  (defun reload-emacs-config ()
+    "Reload the Emacs config from init.el."
+    (interactive)
+    (load-file (expand-file-name "init.el" user-emacs-directory)))
+
   (general-create-definer leader-key
     :states '(normal visual motion emacs)
     :keymaps 'override
@@ -19,16 +29,25 @@
 
   ;; Keep the initial leader map small and close to your Spacemacs muscle memory.
   (leader-key
-    "" '(execute-extended-command :which-key "M-x")
+    "SPC" '(execute-extended-command :which-key "M-x")
+    "0" '(delete-other-windows :which-key "delete other windows")
+    "1" '(delete-window :which-key "delete window")
     "b" '(:ignore t :which-key "buffers")
     "bb" '(consult-buffer :which-key "switch buffer")
     "f" '(:ignore t :which-key "files")
+    "fe" '(:ignore t :which-key "emacs")
+    "fed" '(open-init-file :which-key "open init.el")
+    "fer" '(reload-emacs-config :which-key "reload config")
     "ff" '(find-file :which-key "find file")
     "fs" '(save-buffer :which-key "save file")
     "g" '(:ignore t :which-key "git")
-    "gg" '(magit-status :which-key "magit")
+    "gg" '(open-magit-status-cleanly :which-key "magit")
     "q" '(:ignore t :which-key "quit")
-    "qq" '(save-buffers-kill-terminal :which-key "quit emacs")))
+    "qq" '(save-buffers-kill-terminal :which-key "quit emacs")
+    "w" '(:ignore t :which-key "windows")
+    "wd" '(delete-window :which-key "delete window")
+    "wo" '(delete-other-windows :which-key "delete other windows")
+    "ww" '(other-window :which-key "other window")))
 
 (provide 'keys)
 
